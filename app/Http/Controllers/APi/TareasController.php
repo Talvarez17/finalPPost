256bdbfdcceb5a8f86ab32980d797d83391a99d4
Talvarez17 <soytalvarez17@gmail.com>
2024-04-01 14:16:13 -0600

    Carga final

--- app/Http/Controllers/APi/TareasController.php
+++ app/Http/Controllers/APi/TareasController.php
@@ -13,4 +13,59 @@ class TareasController extends Controller
         $Tarea = Tarea::All();
         return response()->json($Tarea);
     }
+
+    public function getTareaById($id)
+    {
+        $Tarea = Tarea::find($id);
+        return response()->json($Tarea);
+    }
+
+    public function insertTarea(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'descripcion' => 'required',
+            'fechacreacion' => 'required',
+        ]);
+    
+        $Tarea = new Tarea();
+        $Tarea->titulo = $request->titulo;
+        $Tarea->descripcion = $request->descripcion; 
+        $Tarea->fechacreacion = $request->fechacreacion;
+        $Tarea->save();
+    
+        return response()->json([
+            "Model" => $Tarea,
+            "estatus" => 1,
+            "mensaje" => "Tarea registrada"
+        ]);
+    }
+    public function updateTarea(Request $request, $id)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'descripcion' => 'required',
+            'fechacreacion' => 'required',
+        ]);
+    
+        $Tarea = new Tarea();
+        $Tarea->titulo = $request->titulo;
+        $Tarea->descripcion = $request->descripcion; 
+        $Tarea->fechacreacion = $request->fechacreacion;
+        $Tarea->update();
+
+        return response()->json([
+            "Model" => $Tarea,
+            "estatus" => 1,
+            "mensaje" => "Tarea actualizada"
+        ]);
+    }
+    public function deleteTarea($id)
+    {
+        $Tarea = Tarea::find($id)->delete();
+        return response()->json([
+            "estatus" => 1,
+            "mensaje" => "Tarea eliminada"
+        ]);
+    }
 }
